72dde422c6f9b6ef8dc010b4b09640cbf661b0da
RazLands <41165366+RazLands@users.noreply.github.com>
2019-01-17 17:26:56 +0200

    Add files via upload

--- functions.h
+++ functions.h
@@ -3,6 +3,8 @@
 #include <conio.h>
 
 /* Node Stucture */
+typedef char list_type;
+
 typedef struct user_data
 {
 	char name[21];
@@ -78,7 +80,6 @@ void insertLast(list *lst, User data)
 {
 	//create a new link
 	Node *link = (Node*)malloc(sizeof(Node));
-	//Node *current = lst;
 	link->data = data;
 	link->next = NULL;
 	link->prev = NULL;
@@ -99,42 +100,7 @@ void insertLast(list *lst, User data)
 	}
 }
 
-AddUser(list *new)
-{
-	int status, count, check;
-	User data;
-	Node *newuser = (Node*)malloc(sizeof(Node));
-	Node *cmpuser = new->head;
-	printf("Enter user name: ");
-	scanf("%s", newuser->data.name);
-	printf("Enter user code: ");
-	scanf("%s", newuser->data.code);
-	printf("Enter user entrance status: ");
-	scanf("%d", &status);
-	newuser->data.status = status;
-	printf("Enter user permission start date: ");
-	scanf("%s", newuser->data.date_s);
-	printf("Enter user permission end date: ");
-	scanf("%s", newuser->data.date_e);
-	printf("Enter user permission start time: ");
-	scanf("%s", newuser->data.time_s);
-	printf("Enter user permission start time: ");
-	scanf("%s", newuser->data.time_e);
-	count = check = 0;
-	while (cmpuser != NULL)
-	{
-		if (strcmp(cmpuser->data.code, newuser->data.code) > 0) {
-			insertBefore(new, newuser->data, count);
-			check = 1;
-			break;
-		}
-		count++;
-		cmpuser = cmpuser->next;
-	}
-	if (check == 0)
-		insertLast(new, newuser->data);
-}
- /*
+/*
 //delete first item
 int deleteFirst(list *lst)
 {
@@ -207,6 +173,8 @@ int deleteLink(list *lst, int idx)
 	}
 	return 0;
 }
+*/
+
 int insertAfter(list *lst, User data, int idx)
 {
 	if (!isEmpty(lst))
@@ -214,7 +182,7 @@ int insertAfter(list *lst, User data, int idx)
 		int i;
 		//start from the first link
 		Node* current = lst->head;
-		Node* newLink = NULL;
+		Node *newLink = NULL;
 		//navigate through list
 		for (i = 0; i < idx && current != NULL;i++)
 			current = current->next;
@@ -237,15 +205,16 @@ int insertAfter(list *lst, User data, int idx)
 		return 1;
 	}
 	return 0;
-}*/
+}
+
 int insertBefore(list *lst, User data, int idx)
 {
 	if (!isEmpty(lst))
 	{
 		int i;
 		//start from the first link
-		Node *current = lst->head;
-		Node *newLink = NULL;
+		Node* current = lst->head;
+		Node* newLink = NULL;
 		//navigate through list
 		for (i = 0; i < idx && current != NULL;i++)
 			current = current->next;
@@ -267,9 +236,10 @@ int insertBefore(list *lst, User data, int idx)
 		current->prev = newLink;
 		return 1;
 	}
-	//return 0;
+	return 0;
 }
 /* Print all the elements in the linked list */
+
 void print(list *lst) { //Node *head) {
 	Node *current_node = lst->head;
 	while (current_node != NULL) {
@@ -285,6 +255,101 @@ void print(list *lst) { //Node *head) {
 	}
 }
 
+void readAccess(char *path, list *lst) // Node *head) // 
+{
+	/***
+	* Handle function: get's the path of the access file, and print all users information.
+	* You may change it according to your needs.
+	***/
+	FILE *fp;
+	char temp[70], name[21], code[9], date_s[11], date_e[11], time_s[6], time_e[6];
+	int status;
+
+	User d;
+
+	fp = fopen(path, "rb"); //Reads all the data from the file and put it in fp
+	if (!fp)
+	{
+		printf("File not found!\n");
+		return;
+	}
+
+	fgets(temp, 70, fp); //Cuts off the first line (headlines) from fp
+
+	while (fscanf(fp, "%20s %8s %1d %10s %10s %5s %5s %5d", name, code, &status, date_s, date_e, time_s, time_e) != EOF)
+	{
+		strcpy(d.name, name);
+		strcpy(d.code, code);
+		d.status = status;
+		strcpy(d.date_s, date_s);
+		strcpy(d.date_e, date_e);
+		strcpy(d.time_s, time_s);
+		strcpy(d.time_e, time_e);
+
+		//insert_bottom(d, &head);
+		insertLast(lst, d);
+
+	}
+	print(lst);
+	fclose(fp);
+	return lst;
+	//return lst;
+}
+
+void AddUser(list *new)
+{
+	int status, count, check;
+	User data;
+	Node *newuser = (Node*)malloc(sizeof(Node));
+	Node *cmpuser = new->head;
+	printf("Enter user name: ");
+	scanf("%s", newuser->data.name);
+	printf("Enter user code: ");
+	scanf("%s", newuser->data.code);
+	printf("Enter user entrance status: ");
+	scanf("%d", &status);
+	newuser->data.status = status;
+	printf("Enter user permission start date: ");
+	scanf("%s", newuser->data.date_s);
+	printf("Enter user permission end date: ");
+	scanf("%s", newuser->data.date_e);
+	printf("Enter user permission start time: ");
+	scanf("%s", newuser->data.time_s);
+	printf("Enter user permission start time: ");
+	scanf("%s", newuser->data.time_e);
+	count = check = 0;
+	while (cmpuser != NULL)
+	{
+		if (strcmp(cmpuser->data.code, newuser->data.code) > 0) {
+			insertBefore(new, newuser->data, count);
+			check = 1;
+			break;
+		}
+		count++;
+		cmpuser = cmpuser->next;
+	}
+	if (check == 0)
+		insertLast(new, newuser->data);
+}
+
+void search(list *lst, char *name, int status) {
+	Node *current_node = lst->head;
+	list *srch_list = (list *)malloc(sizeof(list));
+	init_list(srch_list);
+
+	if (isEmpty(lst)) {
+		return;
+	}
+
+	while (current_node != NULL) {
+		if (strcmp(current_node->data.name,name) == 0 || current_node->data.status == status)
+			insertLast(srch_list, current_node->data);
+		current_node = current_node->next;
+	}
+	print(srch_list);
+	return srch_list;
+}
+
 int countLines(FILE *fp)
 {
 	//FILE *f = fopen(filename, "r");
@@ -298,9 +363,3 @@ int countLines(FILE *fp)
 	fclose(fp);
 	return count;
 }
-
-
-
-
-
-
--- main.c
+++ main.c
@@ -15,69 +15,11 @@
 * When you submit the program make sure this path match to: "c:\\temp\\log.txt"
 ***/
 
-void readAccess(char *path, list *lst); //Node *head); // 
-/***
-* Handle function: get's the path of the access file, and print all users information.
-* You may change it according to your needs.
-***/
-
-void getDateTime(int *day, int *month, int *year, int *hours, int *mins);
-/***
-* Handle function: Returns by referfance the current date and time
-***/
-
-
-void readAccess(char *path, list *lst) // Node *head) // 
-{
-	FILE *fp;
-	char temp[70], name[21], code[9], date_s[11], date_e[11], time_s[6], time_e[6];
-	int status;
-	  
-	User d;
-
-	fp = fopen(path, "rb"); //Reads all the data from the file and put it in fp
-	if (!fp)
-	{
-		printf("File not found!\n");
-		return;
-	}
-
-	fgets(temp, 70, fp); //Cuts off the first line (headlines) from fp
-
-	while (fscanf(fp, "%20s %8s %1d %10s %10s %5s %5s %5d", name, code, &status, date_s, date_e, time_s, time_e) != EOF)
-	{
-		strcpy(d.name, name);
-		strcpy(d.code, code);
-		d.status = status;
-		strcpy(d.date_s, date_s);
-		strcpy(d.date_e, date_e);
-		strcpy(d.time_s, time_s);
-		strcpy(d.time_e, time_e);
-
-		//printf(
-		//	"First Print:\nname: %s, code: %s, status: %s, date_s: %s, date_e: %s, time_s: %s, time_e: %s\n", 
-		//	d.name,
-		//	d.code,
-		//	d.status,
-		//	d.date_s,
-		//	d.date_e,
-		//	d.time_s,
-		//	d.time_e		
-		//);
-
-		//insert_bottom(d, &head);
-		insertLast(lst, d);
-
-	}
-	print(lst);
-	fclose(fp);
-	return lst;
-	//return lst;
-}
-
-
 void getDateTime(int *day, int *month, int *year, int *hours, int *mins)
 {
+	/***
+	* Handle function: Returns by referfance the current date and time
+	***/
 	time_t rawtime;
 	struct tm *timeinfo;
 
@@ -91,22 +33,6 @@ void getDateTime(int *day, int *month, int *year, int *hours, int *mins)
 	*mins = timeinfo->tm_min;
 }
 
-/*
-node *addUser(node *user) {
-	printf("Enter Name And Code: ");
-	char input[16];
-	fgets(input, 15, stdin);
-	node *newUser = malloc(sizeof(node));
-	sscanf(input, "%s %s", newUser->user.name, newUser->user.code);
-	printf("Added:%s Code:%s\n\n", newUser->user.name, newUser->user.code);
-	newUser->next = NULL;
-	if (user != NULL) {
-		user->next = newUser;
-	}
-	return newUser;
-}*/
-
-
 void main() {
 
 	//Node *head = (Node *)malloc(sizeof(Node));
@@ -115,16 +41,17 @@ void main() {
 	init_list(lst);
 	//lst->head = NULL;
 	//lst->tail = NULL;
-	int option;
+	int option, srch_action, srch_status;
 	char * temp;
+	char srch_name[21];
 
 	//readAccess(ACCESS_PATH, head);
 	readAccess(ACCESS_PATH, lst);
-	AddUser(lst);
-	print(lst);
+	//AddUser(lst);
+	//print(lst);
 	//print(head);
 
-	/* Display Menu
+	/* Display Menu */
 	while (1) {
 		printf("\n *************\n");
 		printf("\n *  Linked list operations:        *\n");
@@ -142,31 +69,25 @@ void main() {
 			continue;
 		}
 		switch (option) {
-		case 1:        // Add to top
-			printf(" Enter a number to insert : ");
-			if (scanf("%d", &num) != 1) {
-				printf(" *Error: Invalid input.\n");
-				scanf("%s", &temp);   //clear input buffer
-				continue;
+		case 1:        // Search for user by its name(s) (press 1) or ststus (2)
+			printf("Choose searching by user NAME (1) or STATUS (2): ");
+			scanf("%d", &srch_action);
+			if (srch_action == 1) {
+				printf("Enter the NAME you want to search for: ");
+				scanf("%20s", srch_name);
+				search(lst, srch_name, 0);
 			}
-			head = insert_top(num, head);
-			printf("Number %d added to the top of the list", num);
-			printf("\nPress any key to continue...");
-			getch();
-			break;
-		case 2:    // add to bottom
-			printf(" Enter a number to insert : ");
-			if (scanf("%d", &num) != 1) {
-				printf(" *Error: Invalid input. \n");
-				scanf("%s", &temp);
-				continue;
+			else if (srch_action == 2) {
+				printf("Enter the STATUS you want to search for: ");
+				scanf("%d", &srch_status);
+				search(lst, "", srch_status);
 			}
-			head = insert_bottom(num, head);
-			printf("%d added to the bottom of the list", num);
-			printf("\nPress any key to continue...");
-			getch();
 			break;
-		case 3:    // Insert After
+		case 2:    // add new user
+			AddUser(lst);
+			print(lst);
+			break;
+/*		case 3:    // Insert After
 			printf(" Enter a number to insert : ");
 			if (scanf("%d", &num) != 1) {
 				printf(" *Error: Invalid input.\n");
@@ -221,13 +142,12 @@ void main() {
 		case 6:  // Exit
 			return(0);
 			break;
-		default:
+*/		default:
 			printf("Invalid Option. Please Try again.");
 			getch();
 		} // End of Switch
 	} // End of While
 	return(0);
-	*/
 
 
 	//newUser = addUser(NULL);
